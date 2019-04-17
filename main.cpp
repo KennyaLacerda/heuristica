f4803465d58049de5a270cc98cd2ae0085f82b9a
kennyalacerda <kennyalacerda7gmail.com>
2019-04-17 17:43:42 -0300

    atualizando

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
-
+#include <ctime>
 #include <cassert>
+#include <time.h>
+using namespace std;
 
 
 std::vector<std::vector<int> > clausulas;
@@ -118,32 +120,95 @@ std::vector<int> buscagsat(std::vector<int> s){
 
 
 
-std::vector<int> walksat(std::vector<int> s){
-    std::vector<int> melhorVizinho = s;
-    int custoMelhorV = coust(s);
-    int empate = 1;
-    std::vector<int> aux = s;
-    int custoaux = custoMelhorV;
-    for(int i = 1; i <= qtdvar; i++){
-      aux[i] = 1 - aux[i];
-        custoaux = coust(aux);
-        //std::cout << custoaux << std::endl;
-        if(custoMelhorV == custoaux){
-            empate++;
-            if(rand()%empate == 0){
-                melhorVizinho = aux;
-                //custoMelhorV = custoaux;
+int custowalksat(int indice, std::vector<int> sol){
+    int custo=0;
+    for(int j=0; j < clausulas[indice].size(); j++){
+        int aux = clausulas[indice][j];
+        if(aux < 0){
+            aux = -aux;
+        }
+        if(clausulas[indice][j] <0 && sol[aux]==0){
+            custo++;
+            break;
+        } else 
+            if(clausulas[indice][j] >0 && sol[aux] == 1) {
+                custo++;
+                break;
             }
-        }
-        else if(custoMelhorV < custoaux){
-          return aux;
-        }
-        aux[i] = 1 - aux[i];
     }
-    return melhorVizinho;
+    return custo; 
 }
 
+
+std::vector<int> insat(vector<int> s){
+    vector<int> insatisfeita;
+    for (int i = 0; i<qtdclausula; i++){
+        if(custowalksat(i,s) == 0){
+            insatisfeita.push_back(i);
+        }
+    }
+    return insatisfeita;
+}
+
+std::vector<int> buscawalksat(std::vector<int> s){
+    std::vector<int> melhorVizinho = s;
+    int custoMelhorV = coust(s);
+    std::vector<int>aux = s;
+    int custoaux = custoMelhorV; 
+    int custoInicial = custoMelhorV;
+    int i =0;
+    vector<int> insatisfeita = insat(s);
+    while(i < insatisfeita.size()){
+        i++;
+        // posição da clausulainsatisfeita
+        int indice;
+        int indicador = rand()%(insatisfeita.size());
+        indice = insatisfeita[indicador];        
+        // percorre todas as variaveis da clausula e retorna variavel da clausula   
+        if(rand()%2==0){            
+             for(int j=0; j<clausulas[indice].size();j++){ 
+                int indaux = clausulas[indice][j];
+                // verifica se esta negativo e torna positivo
+                if(indaux<0){                              
+                    indaux = -indaux;                      
+                }
+                //muda valor da variavel e calcula o custo
+                aux[indaux] = 1 - aux[indaux];
+                custoaux = coust(aux);                
+                if(custoMelhorV < custoaux){
+                    melhorVizinho = aux;
+                    custoMelhorV = custoaux;
+                }
+                aux[indaux] = 1 - aux[indaux];
+            }
+            if(custoInicial <= custoMelhorV){
+                return melhorVizinho;
+            }
+         }else{   //probabilidade de gerar uma variavel aleatoria        
+            int i = rand()%(clausulas[indice].size());
+            int var = clausulas[indice][i];            
+            // verifica se esta negativo e torna positivo
+            if(var < 0){                              
+               var = -var;                      
+            }  
+            //muda valor da variavel e calcula o custo
+            aux[var] = 1 - aux[var];
+            custoaux = coust(aux);              
+            //verifica se a solução melhorou
+            if(custoMelhorV <= custoaux){
+               melhorVizinho = aux;
+               custoMelhorV = custoaux; 
+               return melhorVizinho;
+            }
+            aux[var] = 1 - aux[var];            
+        }
+    }
+    return melhorVizinho;
+}; 
 void gsat(){
+    clock_t t;
+    t = clock();
+    
     int custoMelhorS = 0;
     int coustaux = 0;
     std::vector<int> melhorSol;
@@ -151,7 +216,6 @@ void gsat(){
     while(custoMelhorS < qtdclausula){
         std::vector<int> sol = sol_aleatoria();
         for(int i=0; i< qtdvar*3; i++){
-
             sol =  buscagsat(sol);
             coustaux = coust(sol);
             if(custoMelhorS < coustaux){
@@ -168,19 +232,55 @@ void gsat(){
         inter++;
         std::cout<< custoMelhorS << " interações: " << inter << "\n"; 
     }
+     t = clock() - t;  
+     printf ("(%f seconds)\n",((float)t)/CLOCKS_PER_SEC);
+
+
 };
-
-
-
-
+ void walksat(){clock_t t;
+    t = clock();
+    
+    int custoMelhorS = 0;
+    int coustaux = 0;
+    std::vector<int> melhorSol;
+    int  inter=0; 
+    while(custoMelhorS < qtdclausula){        
+        std::vector<int> sol = sol_aleatoria();
+        for(int i=0; i< qtdvar*3; i++){
+            sol =  buscawalksat(sol);
+            coustaux = coust(sol);
+            if(custoMelhorS < coustaux){
+                melhorSol = sol;
+                custoMelhorS = coustaux;
+            }            
+            if(custoMelhorS == qtdclausula){
+                melhorSol =sol;
+                custoMelhorS = coustaux;
+                std::cout<< custoMelhorS << "\n";                
+                t = clock() - t;  
+                printf ("(%f seconds)\n",((float)t)/CLOCKS_PER_SEC);
+                break;
+            }
+        }
+        inter++;
+        std::cout<< custoMelhorS << " interações: " << inter << "\n";
+        
+    }
+    t = clock() - t;  
+    printf ("(%f seconds)\n",((float)t)/CLOCKS_PER_SEC);
+};
 
 
 int main(int argc, char **argv) {
     //std::vector<std::vector<int> > dados;
     //dados = 
-  srand(0);
-    ler("./flat100-1.cnf");    
-    gsat();       
+    
+   
+    ler("./flat75-1.cnf");
+    srand(25469);    
+    //gsat();
+    walksat();
+      
     /*for(int i=0; i<dados.size(); i++){
        for(int j=0; j<dados[i].size(); j++){
         std::cout<< dados[i][j]<< " ";
